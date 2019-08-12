9c6a9b3b323ed4c8dbfe13ed180210c08946449e
Vaidehi Venkatesan <vaidehi.venkatesan@gmail.com>
2019-08-12 00:06:41 -0400

    modified dampening factor to 2pi instead of pi/2

--- ros/src/twist_controller/yaw_controller.py
+++ ros/src/twist_controller/yaw_controller.py
@@ -3,7 +3,7 @@ import math
 
 MIN_SPEED = 0.1
 VELOCITY_ERROR_MIN_THRESHOLD = 0.0001
-VELOCITY_ERROR_MAX_THRESHOLD = (math.pi * 0.5)
+VELOCITY_ERROR_MAX_THRESHOLD = 2* math.pi
 class YawController(object):
     def __init__(self, wheel_base, steer_ratio, min_speed, max_lat_accel, max_steer_angle):
         self.wheel_base = wheel_base
@@ -27,16 +27,16 @@ class YawController(object):
             max_yaw_rate = abs(self.max_lat_accel / current_velocity);
             angular_velocity = max(-max_yaw_rate, min(max_yaw_rate, angular_velocity))
 
-        #angular_velocity_error = angular_velocity - current_angular_velocity
-        #if abs(angular_velocity_error) < VELOCITY_ERROR_MIN_THRESHOLD: 
-        #    steering_angle = self.last_steering_angle
-        #else: 
-        #    if abs(angular_velocity_error) > VELOCITY_ERROR_MAX_THRESHOLD:
-        #        angular_velocity = angular_velocity - (angular_velocity_error * 0.5) # add dampening factoring by reducing the angular_velocity to make it closer to the current angular velocity
-        #    steering_angle = self.get_angle(max(current_velocity, self.min_speed) / angular_velocity) if abs(angular_velocity) > 0. else 0.0;
-        #    self.last_steering_angle = steering_angle
+        angular_velocity_error = angular_velocity - current_angular_velocity
+        if abs(angular_velocity_error) < VELOCITY_ERROR_MIN_THRESHOLD: 
+            steering_angle = self.last_steering_angle
+        else: 
+            if abs(angular_velocity_error) > VELOCITY_ERROR_MAX_THRESHOLD:
+                angular_velocity = angular_velocity - (angular_velocity_error * 0.5) # add dampening factoring by reducing the angular_velocity to make it closer to the current angular velocity
+            steering_angle = self.get_angle(max(current_velocity, self.min_speed) / angular_velocity) if abs(angular_velocity) > 0. else 0.0;
+            self.last_steering_angle = steering_angle
 
-        steering_angle = self.get_angle(max(current_velocity, self.min_speed) / angular_velocity) if abs(angular_velocity) > 0. else 0.0;
-        self.last_steering_angle = steering_angle
+        #steering_angle = self.get_angle(max(current_velocity, self.min_speed) / angular_velocity) if abs(angular_velocity) > 0. else 0.0;
+        #self.last_steering_angle = steering_angle
     
         return steering_angle
