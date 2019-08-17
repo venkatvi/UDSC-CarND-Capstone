10a0cb0d5377ca0ae39cc01c9887058cf8a38b37
venkatvi <vaidehi.venkatesan@gmail.com>
2019-08-17 01:45:53 -0400

    Add files via upload

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -23,8 +23,8 @@ as well as to verify your TL classifier.
 TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
-LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
-MAX_DECL = 1.0
+LOOKAHEAD_WPS = 120 # Number of waypoints we will publish. You can change this number
+MAX_DECL =  2.0
 
 class WaypointUpdater(object):
     def __init__(self):
@@ -33,8 +33,6 @@ class WaypointUpdater(object):
         rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
         rospy.Subscriber('/traffic_waypoint', Int32, self.traffic_cb)
-
-
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         # TODO: Add other member variables you need below
@@ -46,9 +44,9 @@ class WaypointUpdater(object):
 
         self.run()
     def run(self):
-        rate = rospy.Rate(40)
+        rate = rospy.Rate(50)
         while not rospy.is_shutdown():
-            if self.pose and self.base_waypoints: 
+            if not None in (self.pose, self.base_waypoints): 
                 self.publish_waypoints()
                 
             rate.sleep()
@@ -68,40 +66,34 @@ class WaypointUpdater(object):
 
         val = np.dot(cl_vect-prev_vect, pos_vect - cl_vect)
         if val > 0 :
-            closest_idx = (closest_idx + 1) %len(self.waypoints_2d)
+            closest_idx = (closest_idx + 1) % len(self.waypoints_2d)
         return closest_idx
 
     def publish_waypoints(self):
         final_lane = self.generate_lane()
-        '''
-        # Get closest waypoint 
-        closest_waypoint_idx = self.get_closest_waypoint_idx()
-        self.publish_waypoints(closest_waypoint_idx)
-        lane = Lane()
-        lane.header = self.base_waypoints.header
-        lane.waypoints = self.base_waypoints.waypoints[closest_idx:closest_idx + LOOKAHEAD_WPS]
-        '''
         self.final_waypoints_pub.publish(final_lane)
     def generate_lane(self):
         lane = Lane()
         closest_waypoint_idx = self.get_closest_waypoint_idx()
-        farthest_idx = closest_waypoint_idx + LOOKAHEAD_WPS
-        base_waypoints = self.base_waypoints.waypoints[closest_idx:farthest_idx]
-
+        farthest_idx = closest_waypoint_idx + LOOKAHEAD_WPS 
+        base_waypoints        = self.base_waypoints.waypoints[closest_waypoint_idx:farthest_idx]
         if self.stopline_wp_idx == -1 or (self.stopline_wp_idx >= farthest_idx):
             lane.waypoints = base_waypoints
         else:
-            lane.waypoints = self.decelerate_waypoints(base_waypoints, closest_idx)
-
+            lane.waypoints = self.decelerate_waypoints(base_waypoints, closest_waypoint_idx)
         lane.header = self.base_waypoints.header
         return lane
     def decelerate_waypoints(self, waypoints, closest_idx):
         temp = []
+        stop_idx = max(self.stopline_wp_idx - closest_idx - 2, 0) # 2 waypoints back from the line 
+                        
         for idx, wp in enumerate(waypoints):
             p = Waypoint()
             p.pose = wp.pose
-            stop_idx = max(self.stopline_wp_idx - closest_idx - 2, 0) # 2 waypoints back from the line 
+            
+            # Get total distance between consecutive waypoints from idx to stop_idx
             dist = self.distance(waypoints, idx, stop_idx)
+            
             velocity = math.sqrt(2 * MAX_DECL * dist)
             if velocity < 1.0:
                 velocity = 0.0
@@ -121,8 +113,8 @@ class WaypointUpdater(object):
 
     def traffic_cb(self, msg):
         self.stopline_wp_idx = msg.data
-        
-
+        print("Traffic light found!!! self.stopline_wp_idx = ", self.stopline_wp_idx)
+        pass
     def obstacle_cb(self, msg):
         # TODO: Callback for /obstacle_waypoint message. We will implement it later
         pass
@@ -136,6 +128,11 @@ class WaypointUpdater(object):
     def distance(self, waypoints, wp1, wp2):
         dist = 0
         dl = lambda a, b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2  + (a.z-b.z)**2)
+        if wp2 > len(waypoints):
+            wp2 = len(waypoints)
+        
+        # Compute distance between waypoint i,j for i=1...n-1, j = 2...n where wp1 = 1, wp2 = n 
+        # Sum up all distances 
         for i in range(wp1, wp2+1):
             dist += dl(waypoints[wp1].pose.pose.position, waypoints[i].pose.pose.position)
             wp1 = i
